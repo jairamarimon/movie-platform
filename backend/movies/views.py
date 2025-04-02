9c5b52da6e6743e93e10c4ebac16ec7b39e2305d
Jaira Loi Marimon <marimon.jairaloi@gmail.com>
2025-04-03 00:48:26 +0800

    Initial setup of movie API

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MovieListView(generics.GenericAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            movies = Movie.objects.all()
+            serializer = MovieSerializer(movies, many=True)
+            return Response(serializer.data)
+        except Movie.DoesNotExist:
+            return Response("Movie does not exist")
+
+
+class MovieDetailView(generics.GenericAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
