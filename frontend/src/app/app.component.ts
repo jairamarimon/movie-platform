dac11b4557cddd32bb97a130eea9b1ffe61f42b6
Jaira Loi Marimon <marimon.jairaloi@gmail.com>
2025-04-05 11:13:38 +0800

    Add template for Movies list with navbar, header, table and modal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MovieListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MovieListComponent },
+    { path: 'movie/:id', component: MovieDetailComponent },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
